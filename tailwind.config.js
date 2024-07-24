7fa698532448088315b1a5692f3ac8eedb87c282
Aneer-Thapa1 <anir234thapa@gmail.com>
2024-07-24 10:07:04 +0545

    colors code added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      color: {
+        cardBackground: "#121212",
+        background: "#000000",
+        green: "#1DD25E",
+        gray: "#898989",
+        white: "#F0F0F0",
+      },
+    },
   },
   plugins: [],
-}
-
+};
